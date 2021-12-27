8f1f395f76d8d74cb2751c05766ed7ef04c2422e
FachryRizano <fachry.rizano@gmail.com>
2021-12-27 12:00:08 +0700

    add admin apps

--- lamkprs/settings.py
+++ lamkprs/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'event.apps.EventConfig'
+    'admin.apps.AdminConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
